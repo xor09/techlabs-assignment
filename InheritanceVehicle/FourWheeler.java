52243d5df89e7ff0da8dadb46692e13519ed075e
Bishal Suvechha Manindra <70262740+xor09@users.noreply.github.com>
2023-05-31 20:22:50 +0530

    Create FourWheeler.java

---
+++ InheritanceVehicle/FourWheeler.java
@@ -0,0 +1,14 @@
+package com.techlabs.inheritance;
+
+public class FourWheeler extends Vehicle{
+	double milage;
+
+	public double getMilage() {
+		return milage;
+	}
+
+	public void setMilage(double milage) {
+		this.milage = milage;
+	}
+	
+}
