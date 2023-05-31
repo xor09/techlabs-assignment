9ad4484074a823a8098d5ad230555e271c558178
Bishal Suvechha Manindra <70262740+xor09@users.noreply.github.com>
2023-05-31 20:29:27 +0530

    Create CheckPrimeNumberf.java

---
+++ BasicCoding/CheckPrimeNumberf.java
@@ -0,0 +1,25 @@
+package com.techlabs.assignments;
+
+public class CheckPrimeNumber {
+	public static void main(String[] args) {
+        int number = 17; 
+        boolean isPrime = isPrimeNumber(number);
+        if (isPrime) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+
+    public static boolean isPrimeNumber(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
