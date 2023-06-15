b84823d4573e80eb681012b0a0d3689679475aa4
Bishal Suvechha Manindra <70262740+xor09@users.noreply.github.com>
2023-06-15 16:22:12 +0530

    Create Movie.java

---
+++ Serialization and Deserialization/Movie.java
@@ -0,0 +1,59 @@
+package com.techlabs.serializationAndDeserialization;
+
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+public class Movie implements Serializable{
+	private int id;
+	private String name;
+	private int year;
+	private String genre;
+	
+	public Movie() {}
+	
+	public Movie(int id, String name, int year, String genre) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.year = year;
+		this.genre = genre;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	@Override
+	public String toString() {
+		return "Movie [id=" + id + ", name=" + name + ", year=" + year + ", genre=" + genre + "]";
+	}
+
+}
