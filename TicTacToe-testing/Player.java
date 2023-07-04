82fee7fbc2d67888d8b7f366f074c7e9400bee30
Bishal Suvechha Manindra <70262740+xor09@users.noreply.github.com>
2023-07-04 11:28:14 +0530

    Create Player.java

---
+++ TicTacToe-testing/Player.java
@@ -0,0 +1,18 @@
+
+public class Player {
+	private String name;
+    private MarkType mark;
+
+    public Player(String name, MarkType mark) {
+        this.name = name;
+        this.mark = mark;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public MarkType getMark() {
+        return mark;
+    }
+}
