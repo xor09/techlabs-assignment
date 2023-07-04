62afa836efdf5f929b8df323a2ffd519ddf75f23
Bishal Suvechha Manindra <70262740+xor09@users.noreply.github.com>
2023-07-04 11:31:00 +0530

    Create CellAlreadyMarkedException.java

---
+++ TicTacToe-testing/CellAlreadyMarkedException.java
@@ -0,0 +1,19 @@
+
+
+@SuppressWarnings("serial")
+public class CellAlreadyMarkedException extends Exception{
+	private int row;
+	private int col;
+	private MarkType mark;
+	
+	
+	public CellAlreadyMarkedException(int row, int col, MarkType mark) {
+		this.row = row;
+		this.col = col;
+		this.mark = mark;
+	}
+	
+	public String getMessage() {
+		return "Cell with row " + row + " and col " + col + " already marked with " + mark;
+	}
+}
